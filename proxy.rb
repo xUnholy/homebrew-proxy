904224545e86cac27e4e162e50faa3d71cc08dec
Michael <Michael.Fornaro@anz.com>
2019-02-23 14:26:58 +1100

    updating v0.0.1 official

--- proxy.rb
+++ proxy.rb
@@ -2,7 +2,7 @@ class Proxy < Formula
     desc "A command line tool to configure CNTLM proxy configuration"
     homepage "https://github.com/xUnholy/go-proxy"
     url "https://github.com/xUnholy/go-proxy/releases/download/v0.0.1/darwin-proxy.tar.gz"
-    sha256 "809f9f78ddacf14ecc424daa6a40f8f1f159c362a75a339aae78f7eba69f351e"
+    sha256 "8a4f75f1ecf1bd89f6f4ba0c2b1899ed0b1df8a4a6ac4b9d07aad7f26ebe24c3"
     version "0.0.1"
   
     depends_on "cntlm"
