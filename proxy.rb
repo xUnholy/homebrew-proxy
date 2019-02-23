5d3baf7d304d074787fa1624c121a3893991edce
Michael <Michael.Fornaro@anz.com>
2019-02-23 13:32:02 +1100

    updating tar with new sha

--- proxy.rb
+++ proxy.rb
@@ -2,7 +2,7 @@ class Proxy < Formula
     desc "A command line tool to configure CNTLM proxy configuration"
     homepage "https://github.com/xUnholy/go-proxy"
     url "https://github.com/xUnholy/go-proxy/releases/download/v0.0.1/darwin-proxy.tar.gz"
-    sha256 "f65834030e55f0b890a067e0ed259331a9d26775a7da2f665d7f07399d01a85c"
+    sha256 "809f9f78ddacf14ecc424daa6a40f8f1f159c362a75a339aae78f7eba69f351e"
     version "0.0.1"
   
     depends_on "cntlm"
