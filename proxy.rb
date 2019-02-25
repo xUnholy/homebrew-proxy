937e812641d7c98a1193e81f2a7c9aaf49635e44
Michael <Michael.Fornaro@anz.com>
2019-02-26 10:02:00 +1100

    updating tap to create reqs

--- proxy.rb
+++ proxy.rb
@@ -10,7 +10,29 @@ class Proxy < Formula
     bottle :unneeded
   
     def install
-      bin.install "proxy"
+        bin.install "proxy"
+        bash_profile="#{ENV['HOME']}/.bash_profile"
+        zsh="#{ENV['HOME']}/.zshrc"
+
+        Dir.mkdir("#{ENV['HOME']}/.proxy") unless File.exists?("#{ENV['HOME']}/.proxy")
+        File.open("#{ENV['HOME']}/.proxy/proxy.sh", "w") {} unless File.file?("#{ENV['HOME']}/.proxy/proxy.sh")
+
+        if File.file?(bash_profile)
+            File.open(bash_profile, 'a') { |f| 
+            f.write "\n# Added by CNTLM proxy"
+            f.write "\n[ -f $HOME/.proxy/proxy.sh ] && source $HOME/.proxy/proxy.sh"}
+            puts "Add the following to your bash profile"
+            puts "\techo \"[ -f $HOME/.proxy/proxy.sh ] && source $HOME/.proxy/proxy.sh\" >> ~/.bash_profile"    
+        end
+
+        if File.file?(zsh)
+            File.open(zsh, 'a') { |f| 
+            f.write "\n# Added by CNTLM proxy"
+            f.write "\n[ -f $HOME/.proxy/proxy.sh ] && source $HOME/.proxy/proxy.sh"}
+            puts "Add the following to your zsh profile"
+            puts "\techo \"[ -f $HOME/.proxy/proxy.sh ] && source $HOME/.proxy/proxy.sh\" >> ~/.zshrc"    
+        end
+
     end
-  end
+end
   
