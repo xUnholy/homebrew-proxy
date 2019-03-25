8feca5a85718f972938bab300796400ae155e6ef
Michael <Michael.Fornaro@anz.com>
2019-03-26 09:43:28 +1100

    updating to version 0.1.2

--- proxy.rb
+++ proxy.rb
@@ -2,8 +2,8 @@ class Proxy < Formula
     desc "A command line tool to configure CNTLM proxy configuration"
     homepage "https://github.com/xUnholy/go-proxy"
     url "https://github.com/xUnholy/go-proxy/releases/download/v0.1.0/darwin-proxy.tar.gz"
-    sha256 "22af67ab52cac36ce35f20eff831e8c17e0c83fde900179b9e1aa99a86994888"
-    version "0.1.0"
+    sha256 "c7781ec3e5ff6942d118c20ad048a5b79d1537ac90c321145a74628b9981e691"
+    version "0.1.2"
   
     depends_on "cntlm"
   
@@ -14,23 +14,21 @@ class Proxy < Formula
         bash_profile="#{ENV['HOME']}/.bash_profile"
         zsh="#{ENV['HOME']}/.zshrc"
 
-        Dir.mkdir("#{ENV['HOME']}/.proxy") unless File.exists?("#{ENV['HOME']}/.proxy")
-        File.open("#{ENV['HOME']}/.proxy/proxy.sh", "w") {} unless File.file?("#{ENV['HOME']}/.proxy/proxy.sh")
 
         if File.file?(bash_profile)
             File.open(bash_profile, 'a') { |f| 
             f.write "\n# Added by CNTLM proxy"
-            f.write "\n[ -f $HOME/.proxy/proxy.sh ] && source $HOME/.proxy/proxy.sh"}
+            f.write "\n[ -f $HOME/.proxyrc ] && source $HOME/.proxyrc"}
             puts "Add the following to your bash profile"
-            puts "\techo \"[ -f $HOME/.proxy/proxy.sh ] && source $HOME/.proxy/proxy.sh\" >> ~/.bash_profile"    
+            puts "\techo \"[ -f $HOME/.proxyrc ] && source $HOME/proxyrc\" >> ~/.bash_profile"    
         end
 
         if File.file?(zsh)
             File.open(zsh, 'a') { |f| 
             f.write "\n# Added by CNTLM proxy"
-            f.write "\n[ -f $HOME/.proxy/proxy.sh ] && source $HOME/.proxy/proxy.sh"}
+            f.write "\n[ -f $HOME/.proxyrc ] && source $HOME/.proxyrc"}
             puts "Add the following to your zsh profile"
-            puts "\techo \"[ -f $HOME/.proxy/proxy.sh ] && source $HOME/.proxy/proxy.sh\" >> ~/.zshrc"    
+            puts "\techo \"[ -f $HOME/.proxyrc ] && source $HOME/.proxyrc\" >> ~/.zshrc"    
         end
 
     end
